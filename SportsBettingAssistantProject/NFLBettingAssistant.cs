17f92acf6fdfcbac7489574e3058df7ab3dbe3aa
Matteo DeSimone <thematteod@gmail.com>
2022-10-15 00:44:29 -0400

    Initial commit

---
+++ SportsBettingAssistantProject/NFLBettingAssistant.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+
+namespace SportsBettingAssistantProject
+{
+    internal class NFLBettingAssistant
+    {
+        public static void GetNewData()
+        {
+            /// Generates a new json file at \NFLdata.json
+            //Variables 
+            var API_KEY = ;
+            var SPORT_KEY = "americanfootball_nfl";
+            var REGION = "us";
+            var MARKETS = "spreads";
+            var URL = $"https://api.the-odds-api.com/v4/sports/{SPORT_KEY}/odds/?apiKey={API_KEY}&regions={REGION}&markets={MARKETS}";
+
+
+            // Call API store response
+            using (var client = new HttpClient())
+            {
+                var endpoint = new Uri(URL);
+                var result = client.GetAsync(endpoint).Result.Content.ReadAsStringAsync().Result;
+                File.WriteAllText(@"D:\Repos\SportsBettingAssistant\SportsBettingAssistantProject\bin\Debug\netcoreapp3.1\NFLdata.json", result);
+            }
+
+        }
+        public static void FindBestBets()
+        {
+            //Store JSON to variable
+            var reader = new StreamReader("NFLdata.json");
+            var dataString = reader.ReadToEnd().ToString();
+            var dataJson = JArray.Parse(dataString);
+
+
+            //Loop for each game
+            foreach (var game in dataJson)
+            {
+                //store all bookmakers odds
+                Console.WriteLine($"{game["home_team"]}");
+                //find outliers
+
+                //return selected picks
+            }
+
+        }
+    }
+}
---
+++ SportsBettingAssistantProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using static System.Net.WebRequestMethods;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.IO;
+using File = System.IO.File;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SportsBettingAssistantProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //NFLBettingAssistant.GetNewData();
+            NFLBettingAssistant.FindBestBets();
+        }
+    }
+}
